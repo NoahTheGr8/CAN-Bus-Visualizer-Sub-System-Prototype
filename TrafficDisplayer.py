00f3b2a23b165a4e5ddbd254a3917836f2f6003d
Emiliano-DLC <emiliano_r_d_l_c@outlook.com>
2022-04-15 14:38:57 -0600

    Looks that is not possible to make an autoscroll on treeview (Solution: to have always the most recent element on screen the new elements are put on the first row. recised clumn height and width

--- TrafficDisplayer.py
+++ TrafficDisplayer.py
@@ -1,6 +1,8 @@
+import tkinter
 from tkinter import *
 from tkinter import ttk
 import can
+import time
 
 class GUI(object):
     def __init__(self, title, size):
@@ -14,11 +16,11 @@ class GUI(object):
         self.frame.pack(pady=15)
 
         # Create table
-        self.table = ttk.Treeview(master=self.frame, columns=('Timestamp', 'ID', 'DL', 'DATA', 'Channel'), show='headings')
+        self.table = ttk.Treeview(master=self.frame, columns=('Timestamp', 'ID', 'DL', 'DATA', 'Channel'), show='headings', height=15)
         self.table.pack(side=LEFT)
 
         # Create scrollbar
-        self.scrollbar = Scrollbar(master=self.frame, orient=VERTICAL)
+        self.scrollbar = ttk.Scrollbar(master=self.frame, orient=VERTICAL)
         self.scrollbar.pack(side=RIGHT, fill=Y)
 
         self.table.configure(yscrollcommand=self.scrollbar.set)
@@ -26,11 +28,11 @@ class GUI(object):
 
         # Create columns
         self.table.column('#0', width=0, stretch=NO)
-        self.table.column('#1', anchor=NW)
-        self.table.column('#2', anchor=NW)
-        self.table.column('#3', anchor=NW)
-        self.table.column('#4', anchor=NW)
-        self.table.column('#5', anchor=NW)
+        self.table.column('#1', anchor=CENTER, stretch=NO, width=150)
+        self.table.column('#2', anchor=CENTER, stretch=NO, width=50)
+        self.table.column('#3', anchor=CENTER, stretch=NO, width=50)
+        self.table.column('#4', anchor=CENTER, stretch=NO, width=155)
+        self.table.column('#5', anchor=CENTER, stretch=NO, width=90)
 
         # Create headings
         self.table.heading('#0', text='')
@@ -44,7 +46,10 @@ class GUI(object):
         data = '0x'
         for index in range(0, min(msg.dlc, len(msg.data))):
             data += (f"{msg.data[index]:02x}")
-        self.table.insert(parent='', index='end', values=(msg.timestamp, msg.arbitration_id, msg.dlc, data, msg.channel))
+        # treeview does not support autoscroll so I had to insert the new element at the first row
+        self.table.insert(parent='', index=0, values=(msg.timestamp, msg.arbitration_id, msg.dlc, data, msg.channel))
+        #
+        time.sleep(2)
 
     def start(self):
         self.window.mainloop()
