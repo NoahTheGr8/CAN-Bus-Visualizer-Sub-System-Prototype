b86b500163f8f725810ae16448e39bcb7ba45056
justint9696 <justint9696@hotmail.com>
2022-04-15 13:08:32 -0400

    added right-click menu to edit packet & edit packet window

--- TrafficDisplayer.py
+++ TrafficDisplayer.py
@@ -9,18 +9,30 @@ class GUI(object):
         self.window.title(title)
         self.window.geometry(size)
 
+        # Create menu
+        self.menu = Menu(self.window, tearoff=False)
+        self.menu.add_command(label='Save Packet')
+        self.menu.add_command(label='Edit Packet', command=lambda: self.openWindow(title='Edit Packet', size='500x115'))
+        self.menu.add_command(label='Replay Packet')
+        self.menu.add_command(label='Annotate Packet')
+        self.menu.add_command(label='Delete Packet')
+
+        # Bind right click to menu
+        self.window.bind('<Button-3>', self.openMenu)
+
         # Create frame
-        self.frame = Frame(self.window)
-        self.frame.pack(pady=15)
+        self.frame = Frame(master=self.window)
+        self.frame.pack(padx=15, pady=15, expand=True, fill=BOTH)
 
         # Create table
         self.table = ttk.Treeview(master=self.frame, columns=('Timestamp', 'ID', 'DL', 'DATA', 'Channel'), show='headings')
-        self.table.pack(side=LEFT)
+        self.table.pack(side=LEFT, expand=True, fill=BOTH)
 
         # Create scrollbar
         self.scrollbar = Scrollbar(master=self.frame, orient=VERTICAL)
         self.scrollbar.pack(side=RIGHT, fill=Y)
 
+        # Configure table to scroll vertically with scrollbar
         self.table.configure(yscrollcommand=self.scrollbar.set)
         self.scrollbar.configure(command=self.table.yview)
 
@@ -56,12 +68,73 @@ class GUI(object):
 
         self.window.after(ms, self.messageCallback, ms, function)
 
+    def openMenu(self, e):
+        self.menu.tk_popup(e.x_root, e.y_root)
+
+    def openWindow(self, title, size):
+        # Get tree selected index
+        index = self.table.focus()
+        items = self.table.item(index)
+
+        if len(items['values']) > 0:
+            # Create Top Window
+            self.top_window = Toplevel(self.window)
+            self.top_window.title(title)
+            self.top_window.geometry(size)
+
+            # Create Frame
+            self.top_window.frame = Frame(master=self.top_window)
+
+            # Create Label
+            self.top_window.label = Label(master=self.top_window.frame, text='Edit Packet Data: ', justify=LEFT)
+
+            # Create Text Box
+            self.top_window.text_box = Text(master=self.top_window.frame, height=1, width=int(size.split('x')[0])//12)
+
+            # Pack All to Top Window
+            self.top_window.frame.pack(pady=15)
+            self.top_window.label.pack(side=LEFT)
+            self.top_window.text_box.pack(side=LEFT)
+
+            # Insert current msg.data into text box
+            self.top_window.text_box.insert(INSERT, items['values'][3])
+
+            # Create Button Frame
+            self.top_window.button_frame = Frame(master=self.top_window)
+
+            # Create Buttons - Update and Cancel
+            self.top_window.update = Button(master=self.top_window.button_frame, text='Update Packet',
+                                            command=lambda: self.update(index, self.top_window.text_box, self.top_window))
+            self.top_window.replay = Button(master=self.top_window.button_frame, text='Replay Packet')
+            self.top_window.cancel = Button(master=self.top_window.button_frame, text='Cancel',
+                                            command=self.top_window.destroy)
+
+            # Pack Buttons
+            self.top_window.button_frame.pack(side=BOTTOM, padx=15, pady=15)
+            self.top_window.update.pack(side=LEFT, anchor=E, padx=15)
+            self.top_window.replay.pack(side=LEFT, anchor=E, padx=15)
+            self.top_window.cancel.pack(side=LEFT, anchor=E, padx=15)
+
+            # Lock window size
+            self.top_window.resizable(False, False)
+        else:
+            print('A packet was not selected')
+
+    def update(self, index, text: Text, window: Toplevel=None):
+        items = self.table.item(index)
+        values = items['values']
+        self.table.item(index, text='', values=(values[0], values[1], values[2], text.get(1.0, 'end-1c'), values[4]))
+        if window is not None:
+            window.destroy()
+
+
 class TrafficDisplayer(object):
     def __init__(self, size='1150x200', ms=None, function=None):
         self.gui = GUI('CAN Bus Visualizer', size)
         if function is not None:
             self.gui.messageCallback(ms, function)
         self.gui.start()
+
 
 if __name__ == '__main__':
     disp = TrafficDisplayer()
