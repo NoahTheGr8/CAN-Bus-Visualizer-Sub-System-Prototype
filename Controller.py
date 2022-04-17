960eb80f55604102c1b2b0370e27ad746291acb4
Noah <npizza10@gmail.com>
2022-04-17 16:09:21 -0400

    removed process to automate initiating the vcan interface

--- Controller.py
+++ Controller.py
@@ -25,17 +25,17 @@ if __name__ == '__main__':
     curr_dir = os.getcwd() #current working dir
     script_dir = curr_dir + '/Scripts' #directory with all the scripts
 
-    #Step 1. Load the virtual CAN module, set up virtual interface, and ensure its up and running - vcan_s.sh
-    command1 = './vcan_s.sh'
-    p1 = subprocess.run(command1, shell=True, cwd = script_dir , capture_output=False)
+    #This automates setting up the vcan interface - can possibly be used in software 2
+    #command1 = './vcan_s.sh'
+    #p1 = subprocess.run(command1, shell=True, cwd = script_dir , capture_output=False)
 
-    #Step 2. Nav to ICSim folder and start up the vcan0 (speedometer) - includes ./icsim vcan0
-    command2 = './icsim_s.sh'
+    #Step 1. Nav to ICSim folder and start up the vcan0 (speedometer) - includes ./icsim vcan0
+    command1 = './icsim_s.sh'
+    p1 = subprocess.Popen(command1, shell=True, cwd=script_dir)
+
+    #Step 2. Set up  the CAN Bus Control Panel (xbox controller image) - includes ./controls vcan0
+    command2 = './controls_s.sh'
     p2 = subprocess.Popen(command2, shell=True, cwd=script_dir)
-
-    #Step 3. Set up  the CAN Bus Control Panel (xbox controller image) - includes ./controls vcan0
-    command3 = './controls_s.sh'
-    p3 = subprocess.Popen(command3, shell=True, cwd=script_dir)
 
     # run() starts the GUI and the reading of the packets
     run()
